e46df3f5bfb543687542ca4bfb04504988254827
Larry Fenn <larry.fenn@gmail.com>
2023-05-06 23:44:50 -0400

    Cut down on requests and make behavior once-a-day

--- check.py
+++ check.py
@@ -1,7 +1,10 @@
 import requests
 import time
+from suntime import Sun
+from datetime import datetime
 
 session = requests.Session()
+sun = Sun(40.72118, -73.98347)
 
 def send_command(action):
     url = f'http://192.168.1.174:8838/neo/v1/transmit?command=008.211-15-{action}&id=CL8oIvtpXVGR_QSd9iOgHA'
@@ -14,12 +17,28 @@ def query_sensor(id):
     data = session.get(url, timeout=5).content
     return int(data)
 
+def check_sun_up():
+    sunrise = sun.get_local_sunrise_time()
+    sunset = sun.get_local_sunset_time()
+    now = datetime.now().astimezone()
+    return (now > sunrise) & (now < sunset)
+
+can_close = True
+can_open = True
+
 while True:
+    sun_up = check_sun_state()
     mainroom_co2 = query_sensor('8a93c7')
     bedroom_co2 = query_sensor('dd58e7')
     diff = bedroom_co2 - mainroom_co2
-    if diff > 100:
+    close_condition = diff > 50
+    open_condition = diff < 25
+    if can_close and close_condition and not sun_up:
+        can_close = False
+        can_open = True
         send_command('dn')
-    if diff < 50:
+    if can_open and open_condition and sun_up:
+        can_open = False
+        can_close = True
         send_command('up')
     time.sleep(15)
